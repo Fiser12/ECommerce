d1f8230718d273154e1c2b33a1d7ddffa54bacd7
Fiser <fiser12@icloud.com>
2018-12-21 21:47:57 +0100

    FIX Change named of the variables and rename correct namespace

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20180226142349.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20180226142349.php
@@ -1,8 +1,17 @@
 <?php
 
+/*
+ * This file is part of the Php DDD Standard project project.
+ *
+ * Copyright (c) 2017-present-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
-namespace Sylius\Migrations;
+namespace ECommerce\Infrastructure\Persistence\Doctrine\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
@@ -15,14 +24,16 @@ class Version20180226142349 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
         $this->addSql('ALTER TABLE sylius_product_review CHANGE title title VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
         $this->addSql('ALTER TABLE sylius_product_review CHANGE title title VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
     }
 }
